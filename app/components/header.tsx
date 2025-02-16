fa38977e2ed1602fb2f3104595b424a0e825231a
fisayo-dev <olufisayobadina@gmail.com>
2025-02-16 09:11:36 +0100

    using nect link

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,14 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 const header = () => {
   return (
       <div className='mx-auto container'>
-        <div className="py-2">
+        <div className="py-6 px-2">
             <div className="flex items-center justify-between">
                 <h2 className="text-3xl font-bold">Github Searcher</h2>
                 <div className="flex items-center space-x-2">
-                    <a href="#" className="hover:bg-zinc-300">Home</a>
-                    <a href="#" className="hover:bg-zinc-300">Search</a>
+                    <Link href="/" className="hover:text-zinc-300">Home</Link>
+                    <Link href="/about" className="hover:text-zinc-300">Search</Link>
                   </div>
               </div>
           </div>
