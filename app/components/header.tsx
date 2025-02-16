bb61e1bdf1a9fba0ab1ede02d6dae7b8e808e51c
fisayo-dev <olufisayobadina@gmail.com>
2025-02-16 09:14:45 +0100

    use search route

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -9,7 +9,7 @@ const header = () => {
                 <h2 className="text-3xl font-bold">Github Searcher</h2>
                 <div className="flex items-center space-x-2">
                     <Link href="/" className="hover:text-zinc-300">Home</Link>
-                    <Link href="/about" className="hover:text-zinc-300">Search</Link>
+                    <Link href="/search" className="hover:text-zinc-300">Search</Link>
                   </div>
               </div>
           </div>
